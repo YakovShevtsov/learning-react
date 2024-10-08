9076db280e66a8ee9598a5054feedede52f6fcd6
Yakov <yakov.shev@gmail.com>
2024-10-08 02:29:55 +0200

    Moved components to separated files

---
+++ 03-React-Essentials/src/components/CoreConcept.jsx
@@ -0,0 +1,13 @@
+function CoreConcept({title, description, image}) {
+    return (
+      <div>
+        <li>
+          <img src={image} alt={title} />
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </li>
+      </div>
+    )
+  }
+
+export default CoreConcept;
