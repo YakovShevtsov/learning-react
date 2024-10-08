fec853588a7ce313ecde8a44b7842daa5f881abc
Yakov <yakov.shev@gmail.com>
2024-10-08 02:12:48 +0200

    Added data.js with core concepts array. Alternative way to define/use props

--- 03-React-Essentials/src/App.jsx
+++ 03-React-Essentials/src/App.jsx
@@ -1,6 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
-import componentsImg from './assets/components.png';
-
+import { CORE_CONCEPTS } from './data';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -8,13 +7,13 @@ function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
-function CoreConcept(props) {
+function CoreConcept({title, description, image}) {
   return (
     <div>
       <li>
-        <img src={props.image} alt={props.title} />
-        <h3>{props.title}</h3>
-        <p>{props.description}</p>
+        <img src={image} alt={title} />
+        <h3>{title}</h3>
+        <p>{description}</p>
       </li>
     </div>
   )
@@ -43,9 +42,10 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcept title="Components" description="The core ui building block" image={componentsImg}/>
-            <CoreConcept title="" description="" />
-            <CoreConcept title="" description="" />
+            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
         </section>
       </main>
