185e8946189bc97677dab42a5a9518a41f4ce7d5
Yakov <yakov.shev@gmail.com>
2024-10-08 23:19:58 +0200

    Passed custom arguments to event function through onSelect prop

--- 03-React-Essentials/src/App.jsx
+++ 03-React-Essentials/src/App.jsx
@@ -4,8 +4,10 @@ import CoreConcept from "./components/CoreConcept";
 import TabButton from "./components/TabButton";
 
 function App() {
-  function handleClick() {
-    console.log("Hello world! - selected");
+  function handleClick(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state'
+    console.log(selectedButton);
+    
   }
 
   return (
@@ -28,10 +30,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleClick}>Components</TabButton>
-            <TabButton onSelect={handleClick}>JSX</TabButton>
-            <TabButton onSelect={handleClick}>Props</TabButton>
-            <TabButton onSelect={handleClick}>State</TabButton>
+            <TabButton onSelect={() => handleClick('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleClick('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleClick('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleClick('state')}>State</TabButton>
           </menu>
         </section>
       </main>
