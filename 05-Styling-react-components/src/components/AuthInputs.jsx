ca47f1395854a7503597a5cf10e8bf1891e061ea
Yakov <yakov.shev@gmail.com>
2024-11-03 16:40:37 +0100

    Creating Reusable Components & Component Combinations

--- 05-Styling-react-components/src/components/AuthInputs.jsx
+++ 05-Styling-react-components/src/components/AuthInputs.jsx
@@ -1,47 +1,13 @@
 import { useState } from "react";
 import classes from "styled-components";
+import Button from "./Button";
+import InputLabel from "./InputLabel";
 
 const ControlDiv = classes.div`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`;
-
-const Label = classes.label`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
-`;
-
-const Input = classes.input`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
-  color: ${({ $invalid }) => ($invalid ? "#ef4444" : "#374151")};
-  border: 1px solid ${({ $invalid }) => ($invalid ? "#f73f3f" : "transparent")};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
-
-const Button = classes.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
-
-  &:hover {
-    background-color: #f0920e;s
-  }
 `;
 
 export default function AuthInputs() {
@@ -68,9 +34,9 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlDiv>
         <p className="paragraph">
-          <Label $invalid={emailNotValid}>Email</Label>
-          <Input
-            $invalid={emailNotValid}
+          <InputLabel
+            invalid={emailNotValid}
+            labelText="Email"
             type="email"
             // style={{
             //   backgroundColor: emailNotValid ? "#fed2d2" : "#d1d5db",
@@ -79,9 +45,9 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <Label $invalid={passwordNotValid}>Password</Label>
-          <Input
-            $invalid={passwordNotValid}
+          <InputLabel
+            invalid={passwordNotValid}
+            labelText="Password"
             type="password"
             className={passwordNotValid ? "invalid" : undefined}
             onChange={(event) =>
