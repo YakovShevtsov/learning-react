adc4bcd4649b63fa06cac02d0da7d691e6761071
Yakov <yakov.shev@gmail.com>
2024-11-30 17:28:13 +0100

    Using useCallback with memo() to prevent component rerenders in case of props as a fuctions

--- 09-React-behind-the-scenes/src/components/UI/IconButton.jsx
+++ 09-React-behind-the-scenes/src/components/UI/IconButton.jsx
@@ -1,13 +1,19 @@
-import { log } from '../../log.js';
+import { log } from "../../log.js";
+import { memo } from "react";
 
-export default function IconButton({ children, icon, ...props }) {
-  log('<IconButton /> rendered', 2);
+const IconButton = memo(function IconButton({ children, icon, ...props }) {
+  log("<IconButton /> rendered", 2);
 
   const Icon = icon;
   return (
-    <button {...props} className="button">
+    <button
+      {...props}
+      className="button"
+    >
       <Icon className="button-icon" />
       <span className="button-text">{children}</span>
     </button>
   );
-}
+});
+
+export default IconButton;
