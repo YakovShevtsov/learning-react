5de29040fd5b83be3633ea57f6ec0f553476cb11
Yakov <yakov.shev@gmail.com>
2025-02-18 02:13:34 +0100

    Outsourced 'slices' code to separate files

---
+++ 14-Diving-into-Redux/src/store/auth-slice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authInitialState = {
+  isLoggedIn: false,
+  userData: { email: "test@example.com", password: "12345" },
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: authInitialState,
+  reducers: {
+    login(state, action) {
+      if (
+        action.payload.enteredEmail === state.userData.email &&
+        action.payload.enteredPassword === state.userData.password
+      ) {
+        state.isLoggedIn = true;
+      }
+    },
+
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+});
+
+export default authSlice;
--- 14-Diving-into-Redux/src/store/index.js
+++ 14-Diving-into-Redux/src/store/index.js
@@ -1,48 +1,6 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const counterInitialState = { counter: 0, showCounter: true };
-const authInitialState = {
-  isLoggedIn: false,
-  userData: { email: "test@example.com", password: "12345" },
-};
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: counterInitialState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter += action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const authSlice = createSlice({
-  name: "auth",
-  initialState: authInitialState,
-  reducers: {
-    login(state, action) {
-      if (
-        action.payload.enteredEmail === state.userData.email &&
-        action.payload.enteredPassword === state.userData.password
-      ) {
-        state.isLoggedIn = true;
-      }
-    },
-
-    logout(state) {
-      state.isLoggedIn = false;
-    },
-  },
-});
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counter-slice";
+import authSlice from "./auth-slice";
 
 const store = configureStore({
   reducer: {
