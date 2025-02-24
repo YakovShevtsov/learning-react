342223c2589d2f8aa00d2e7b924d058c1149643f
Yakov <yakov.shev@gmail.com>
2025-02-24 23:55:39 +0100

    Defining second route

--- 16-React-Router/src/App.js
+++ 16-React-Router/src/App.js
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
+import Products from "./pages/Products";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <HomePage />,
+    },
+    {
+      path: "/products",
+      element: <Products />,
     },
   ]);
 
