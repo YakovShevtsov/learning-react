54d780651a53123606f644a02eac62a5390ee553
Yakov <yakov.shev@gmail.com>
2025-06-07 03:15:45 +0200

    Data Fetching with a loader() & Using Data From A Loader In The Route Component

--- 16-React-Router/part-2/frontend/src/pages/EventsPage.jsx
+++ 16-React-Router/part-2/frontend/src/pages/EventsPage.jsx
@@ -1,27 +1,8 @@
-import { Link } from "react-router";
-
-const DUMMY_EVENTS = [
-  {
-    id: "e1",
-    title: "A dummy event",
-    date: "2023-02-22",
-    image:
-      "https://blog.hubspot.de/hubfs/Germany/Blog_images/Optimize_Marketing%20Events%20DACH%202021.jpg",
-    description: "Join this amazing event and connect with fellow developers.",
-  },
-];
+import EventsList from "../components/EventsList";
+import { useLoaderData } from "react-router";
 
 export function EventsPage() {
-  return (
-    <>
-      <h1>Events page!</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  const eventsData = useLoaderData();
+
+  return <EventsList events={eventsData} />;
 }
